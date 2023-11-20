7c3aaa49525e7e12f84bb5b772a781523d66d6e5
Descamil <73858568+Daniel404kali@users.noreply.github.com>
2023-11-20 18:57:40 +0100

    Add files via upload

--- GNL/GNL2/get_next_line.c
+++ GNL/GNL2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/05 16:00:27 by descamil          #+#    #+#             */
-/*   Updated: 2023/11/18 18:41:47 by descamil         ###   ########.fr       */
+/*   Updated: 2023/11/20 18:56:53 by descamil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ char	*get_next_line(int fd)
 	int			bytesread;
 	int			i;
 	int			j;
-	int			k;
 
 	i = 0;
 	j = 0;
@@ -74,50 +73,36 @@ char	*get_next_line(int fd)
 			if (bytesread < 0)
 				return (NULL);
 			buffer[bytesread + 1] = '\0';									// Le pongo '\0' al final para poder iterar
-			k = 0;
 			tmp = (char *)malloc(bytesread + 1);									// Asigno memoria a tmp para que entre el buffer
 			if (tmp == NULL)
 				return (NULL);
+			tmp = ft_memcpy(tmp, buffer, bytesread + 1);
 		}
-		else if (buffer != NULL)									// En caso de que el buffer tenga contenido
+		else if (ft_strchr(buffer, '\n') == 1)					// En caso de que el buffer tenga contenido
 		{
-			// * [if (ft_strchr(tmp, '\n') == 0)] * //
-			if (ft_strchr(buffer, '\n') == 0)							// Si no hay saltos de linea en tmp
-			{
-				tmp = ft_memcpy(tmp, buffer, ft_strlen(buffer) + 1);			// Copiamos el contenido del buffer en el tmp
-				bytesread = read(fd, buffer, BUFFER_SIZE);					// Cogemos nuevo contenido
-				while (i < bytesread && buffer[i] != '\n')					// Hasta que llegue al primer '\n'
-				{
-					tmp[ft_strlen(tmp) + i] = buffer[i];					// Copiamos el contenido nuevo en el tmp
-					i++;
-				}
+			tmp = NULL;
+			bytesread = ft_strlen(buffer);									// Asignamos el tamaño de tmp
+			while (buffer[i] != '\n' && ft_strchr(buffer, '\n') == 1)		// Buscamos el salto de linea
 				i++;
-				while (i < bytesread)										// Si despues del '\n' hay más contenido
-					buffer[j++] = buffer[i++];								// Se copia en el mismo buffer para que vuelva arriba
-				while (j < i)
-					buffer[j++] = '\0';
-			}
-			else													// Si hay saltos de liena en tmp
+			tmp = (char *)malloc(i + 1);							// Asgnamos memoria a str_old
+			if (tmp == NULL)
+				return(NULL);
+			i = 0;
+			while(buffer[i] != '\n')
 			{
-				tmp = NULL;
-				bytesread = ft_strlen(buffer);									// Asignamos el tamaño de tmp
-				while (buffer[i] != '\n')										// Buscamos el salto de linea
-					i++;
-				tmp = (char *)malloc(i + 1);							// Asgnamos memoria a str_old
-				if (tmp == NULL)
-					return(NULL);
-				i = 0;
-				while (buffer[i] != '\n')										// Mientras que no se encuentre el salto de linea
-				{
-					tmp[i] = buffer[i];									// Copio los valores antes del salto de linea
-					i++;
-				}
-				i++;														// Saltamos el salto de linea
-				while (buffer[i] != '\0')
-					buffer[j++] = buffer[i++];									// Si quedan más caracteres, los despazo hacia delante
-				while (j < i)
-					buffer[j++] = '\0';										// El espacio sobrante, los sustituyo por '\0'
+				tmp[i] = buffer[i];									// Copio los valores antes del salto de linea
+				i++;
 			}
+			i++;														// Saltamos el salto de linea
+			while (i < bytesread)
+				buffer[j++] = buffer[i++];									// Si quedan más caracteres, los despazo hacia delante
+			while (j < bytesread)
+				buffer[j++] = '\0';										// El espacio sobrante, los sustituyo por '\0'
+		}
+		else if (ft_strchr(buffer, '\n') == 0)
+		{
+			tmp = (char *)malloc(ft_strlen(buffer) + 1);
+			tmp = ft_memcpy(tmp, buffer, ft_strlen(buffer));
 		}
 		if (str == NULL)											// Combrobamos que str es NULL o k es 1
 		{
@@ -125,20 +110,7 @@ char	*get_next_line(int fd)
 			if (str == NULL)
 				return (NULL);
 		}
-		if (tmp != NULL)											// Si tmp tiene memoria creada
-		{
-			while (i < bytesread)
-			{
-				tmp[i] = buffer[i];											// Copiamos los datos del buffer en tmp
-				if (buffer[i] == '\n')										// Buscamos el salto de linea
-				{
-					i++;
-					break ;
-				}
-				i++;
-			}
-		}
-		if (ft_strchr(buffer, '\0') == 0)											// Si el salto de linea está al final
+		if (ft_strchr(buffer, '\0') == 1 && ft_strchr(buffer, '\n') == 0)	// Si en el buffer hay un '\0' y no un '\n'
 		{
 			str_old = str;													// Asignamos el contenido de str en str_old
 			str = ft_strjoin(str_old, tmp);									// Juntamos las cadenas str_old y tmp en lla cadena str
@@ -154,30 +126,38 @@ char	*get_next_line(int fd)
 		else if (ft_strchr(buffer, '\n' == 1))										// Si el salto de linea no esta al final
 		{
 			j = 0;
-			if (tmp != NULL)												// Si tmp tiene contenido
+			if (tmp != NULL && ft_strchr(tmp, '\n') == 0)												// Si tmp tiene contenido
 			{
 				str_old = str;
 				str = ft_strjoin(str_old, tmp);								// Juntamos el contenido de str_old con tmp en str
+				if (str == NULL)
+					return (NULL);
 			}
-			if (str == NULL)
-				return (NULL);
-			if (i < bytesread)
+			if (ft_strchr(str, '\n') == 0 && ft_strchr(tmp, '\n') == 1)			// Si str no es NULL y no tine un salto de linea
 			{
-				while (i < bytesread)											// Si quedan más caracteres, los despazo hacia delante
-					buffer[j++] = buffer[i++];
-				while (j <= bytesread)											// El espacio sobrante, los sustituyo por '\0'
-					buffer[j++] = '\0';
+				while (ft_strchr(str, '\n') == 0)
+				{
+					if (tmp[i] == '\n')
+						str[ft_strlen(str)] = '\n';
+					else
+						str[ft_strlen(str)] = tmp[i];
+					i++;														// Se le añade uno
+				}
 			}
-			if (str != NULL && ft_strchr(str, '\n') == 0)			// Si str no es NULL y no tine un salto de linea
-				str[ft_strlen(str)] = '\n';									// Se le añade uno
+			while (i < bytesread)												// Si quedan más caracteres, los despazo hacia delante
+				buffer[j++] = buffer[i++];
+			while (j < bytesread)												// El espacio sobrante, los sustituyo por '\0'
+				buffer[j++] = '\0';
+			if (buffer[0] == '\0')
+				buffer = NULL;
 			tmp = NULL;
 			str_old = NULL;
-			free(tmp);														// Reiniciamos valores
+			free(tmp);																// Reiniciamos valores
 			free(str_old);
-			break ;															// Salimos del while
+			break ;																	// Salimos del while
 		}
 	}
-	return (str);													// Devolvemos la cadena final
+	return (str);																	// Devolvemos la cadena final
 }
 
 int main()
@@ -186,7 +166,7 @@ int main()
 	char	*line;
 	int		i;
 
-	i = 3;
+	i = 10;
 	fd = open("text.txt", O_RDONLY);
 	if (fd == -1)
 		return (1);
