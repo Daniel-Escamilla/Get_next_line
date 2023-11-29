d5dbb4bcb0816625cc102cf18016da3c4f9cf997
Descamil <73858568+Daniel404kali@users.noreply.github.com>
2023-11-29 18:41:05 +0100

    Add files via upload

---
+++ GNL/GNL3/get_next_line.c
@@ -0,0 +1,182 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Esquema.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 12:46:44 by descamil          #+#    #+#             */
+/*   Updated: 2023/11/27 17:27:54 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_buffer_move(char *buffer, int i, int j, char *tmp)
+{
+	if ((ft_strchr(buffer, '\n') == 1 || ft_strchr(buffer, '\0') == 1)
+		&& i < ft_strlen(buffer))
+	{
+		while ((buffer[i] != '\0' && buffer[i] != '\n') && i < ft_strlen(buffer))
+		{
+			tmp[i] = buffer[i];
+			i++;
+		}
+		if (buffer[i] == '\n')
+			tmp[i++] = '\n';
+		tmp[i] = '\0';
+	}
+	while (i < BUFFER_SIZE)
+		buffer[j++] = buffer[i++];
+	while (j < i)
+		buffer[j++] = '\0';
+	// if (buffer[0] == '\0')
+	// 	free(buffer);
+	return (buffer);
+}
+
+char	*ft_read(int fd, char *buffer)
+{
+	int	bytes_read;
+
+	buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (buffer == NULL)
+	{
+		free (buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read < 0)
+	{
+		free (buffer);
+		buffer = NULL;
+		return (NULL);
+	}
+	if (bytes_read == 0)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+	if (buffer != NULL)
+		buffer[bytes_read] = '\0';
+	return (buffer);
+}
+
+char	*ft_buffer_extract(char *buffer, char *line)
+{
+	char	*tmp;
+
+	tmp = NULL;
+	if (buffer == NULL)
+		return (line);
+	tmp = (char *)ft_calloc(sizeof(char), BUFFER_SIZE + 1);
+	buffer = ft_buffer_move(buffer, 0, 0, tmp);
+	line = ft_strjoin(line, tmp);
+	if (buffer[0] == '\0')
+		buffer = NULL;
+	free (tmp);
+	tmp = NULL;
+	return (line);
+}
+
+char	*ft_first_line(char *buffer, int fd)
+{
+	char	*line;
+	int		i;
+
+	line = NULL;
+	i = 0;
+	while (1)
+	{
+		if (ft_strchr(buffer, '\n') == 1)
+			break ;
+		else if ((ft_strlen(buffer) < BUFFER_SIZE && ft_strchr(buffer, '\n') == 0) || BUFFER_SIZE == 1 || i == 2)
+		{
+			i = 0;
+			line = ft_strjoin(line, buffer);
+			free(buffer);
+			buffer = NULL;
+			buffer = ft_read(fd, buffer);
+			if (buffer == NULL)
+			{
+				free(buffer);
+				break ;
+			}
+		}
+		if ((ft_strlen(buffer) < BUFFER_SIZE && (ft_strchr(buffer, '\n') == 0)))
+			break ;
+		i++;
+	}
+	line = ft_buffer_extract(buffer, line);
+	if (buffer != NULL && buffer[0] == '\0')
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*line;
+	int			i;
+	int			j;
+
+	i = 0;
+	j = 0;
+	line = NULL;
+	if (buffer)
+	{
+		if (buffer[i] == '\0')
+			buffer = NULL;
+	}
+	if (!buffer)
+	{
+		buffer = ft_read(fd, buffer);
+		if (buffer == NULL)
+			return (NULL);
+		line = ft_first_line(buffer, fd);
+	}
+	else
+		line = ft_first_line(buffer, fd);
+	if (buffer == NULL)
+		return (line);
+	if (line == NULL)
+	{	
+		free(line);
+		line = NULL;
+		buffer = NULL;	
+		return (NULL);
+	}
+	return (line);
+}
+
+
+// void runleaks(void)
+// {
+//    system("leaks a.out");
+// }
+
+// int main()
+// {
+// 	int		fd;
+// 	char	*line;
+// 	int		i;
+
+// 	i = 2;
+// 	fd = open("1char.txt", O_RDONLY);
+// 	if (fd == -1)
+// 		return (1);
+// 	while (i > 0)
+// 	{
+// 		line = (get_next_line(fd));
+// 		printf("%s",line);
+// 		free(line);
+// 		i--;
+// 	}
+// 	// atexit(runleaks);
+// 	close (fd);
+// 	return (0);
+// }
---
+++ GNL/GNL3/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: descamil <descamil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 16:35:10 by descamil          #+#    #+#             */
+/*   Updated: 2023/11/27 17:28:15 by descamil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <limits.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+int		ft_strlen(const char *s);
+int     ft_strchr(const char *s, char c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char    *ft_clean_buffer(char *buffer);
+void	*ft_calloc(size_t nmemb, size_t size);
+
+#endif
